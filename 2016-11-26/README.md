282dacd5c64f976428c7f9e0d8ce70cf60b1dbaf
Humberto Rocha <humrochagf@gmail.com>
2016-11-26 13:34:22 -0200

    dados do dojo de 26 de novembro de 2016

--- 2016-11-26/README.md
+++ 2016-11-26/README.md
@@ -4,16 +4,38 @@ Implementar uma função que recebe uma matriz 50X50 como seed, aplique as regra
 
 
 ## Prós
-* Não preenchido
+* Foi divertido
+* Boa interação
+* Diversidade de raciocínios lógicos
+* Teve bolo (cake is not a lie)
+* Novos membros
 
 
 ## Contras
-* Não preenchido
-
+* Dispersões eventuais
+* Nem todos programaram
+* Tela dificil de visualizar
 
 ## Sugestões
-* Não preenchido
+* Colocar a tela em vertical para melhor visualização ou usar um projetor
+* Encontros mais frequentes
 
 
 ## Participantes
 * [Humberto Rocha](https://github.com/humrochagf)
+* [Tânia Andrea](https://github.com/taniaa)
+* [Bruno Barbosa](https://github.com/brunobbbs)
+* [Rogério Fonseca Gonçalves](https://github.com/rogeriofonseca)
+* [Bleno Silva](https://github.com/bleno)
+* [Théo A. Monteiro](https://github.com/theoamonteiro)
+* [Starlone Passos](https://github.com/starlone)
+* [Rogério Fernandes Pereira - FOX](https://github.com/rogeriofox)
+* [Jéssica Amaral](https://github.com/jornalistadigital)
+* [Gabriel Rocha Ferreira]
+* [Marco Rougeth](https://github.com/rougeth)
+* [Felipe Brum Araújo]
+* [Thiago de Souza]
+* [Bernardo Alves Furtado]
+* [Arthur Mota Furtado]
+* [Fabiana Ganem](https://github.com/fabianagoa)
+* [Marcos]
--- 2016-11-26/game.py
+++ 2016-11-26/game.py
@@ -2,12 +2,55 @@
 
 import unittest
 
+matrix = [[0, 0, 0],
+          [0, 1, 0],
+          [0, 0, 1]]
 
-def exemplo():
-    return True
+matrix2 = [[0, 0, 0],
+          [0, 0, 1],
+          [1, 0, 1]]
 
+matrix3 = [[0, 0, 1],
+          [0, 1, 1],
+          [1, 0, 1]]
+
+matrix4 = [[0, 0, 0],
+           [0, 1, 1],
+           [1, 0, 1]]
+
+matrix5 = [[0, 0, 0],
+           [0, 1, 0],
+           [0, 0, 0]]
+
+def regra1(matrix):
+    if soma_matrix(matrix) < 2:
+        return 0
+
+def regra2(matrix):
+    if soma_matrix(matrix) > 3:
+        return 0
+def regra3(matrix):
+    return 1 if soma_matrix(matrix) == 3 else 0 
+
+
+def soma_matrix(matrix):
+    resultado = -1*matrix[1][1]
+    for line in matrix:
+        resultado += sum(line)
+    return resultado
 
 class TestGameOfLife(unittest.TestCase):
 
-    def testExemplo(self):
-        return self.assertTrue(exemplo())
+    def testRegra1_menos_de_dois_morre(self):
+        return self.assertEqual(regra1(matrix), 0)
+
+    def testRegra2_mais_de_tres_morre(self):
+        return self.assertEqual(regra2(matrix3), 0)
+
+    def testRegra3_igual_a_tres_vive(self):
+        return self.assertEqual(regra3(matrix2), 1)
+
+    def testRegra3_igual_a_tres_vive_com_meio_vivo(self):
+        return self.assertEqual(regra3(matrix4), 1)    
+
+        
